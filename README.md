55f95920bfbfaaf127ec5afc8f62510b4be0b167
cleryc <cleryc@tcd.ie>
2019-02-07 23:05:50 +0000

    Finished work on parser and experiments with ssl certs

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # nodejs-proxy-server
 
 The  objective  of  the  exercise  is  to  implement  a  Web  Proxy  Server.  A  Web  proxy  is  a localserver, which  fetches  items  from  the  Web  on  behalf  of  a  Web  client  instead  of  the  client  fetching  them directly. This allows for caching of pages and access control.The program should be able to:1.Respond  to  HTTP &  HTTPS requests,and  should  display  eachrequest  on  a  management console.  It  should  forward  the  request  to  the  Web  server  and  relay  the  response  to  the browser.2.Handle websocket connections.3.Dynamically blockselected URLsvia the management console.4.Efficiently cache  requestslocally  and  thus  save  bandwidth.You  must  gather  timing  and bandwidth data to prove the efficiency of your proxy.5.Handle multiple requests simultaneouslyby implementing a threaded server.The program can be written in a programming language of your choice.However, you must ensure that you do not overuse any API or Library functionality that implements the majority of the work for you.
+
+
+Generating private key: $ openssl genrsa -out private-key.pem 1024
+
+Generating CSR:         $ openssl req -new -key private-key.pem -out csr.pem
+
+Public Cert:            $ openssl x509 -req -in csr.pem -signkey private-key.pem -out public-cert.pem
---
+++ index.js
@@ -0,0 +1,84 @@
+var express = require("express");
+var request = require("request");
+var fs = require("fs");
+var tls = require("tls");
+var net = require("net");
+
+var app = express();
+
+var options = {
+  key: fs.readFileSync('private-key.pem'),
+  cert: fs.readFileSync('public-cert.pem')
+};
+
+const LOCAL_PORT  = 6512;
+const REMOTE_PORT = 80;
+const REMOTE_ADDR = "www.lolcats.com";
+
+
+var httpServer = net.createServer(function (socket) {
+    socket.on('data', function (msg) {
+        //parseClientRequest Currently does nothing but is necessary for mapping host name to remote socket
+        //also necessary for maintaining forbidden host list
+        clientRequest = parseClientRequest(msg);
+
+        
+        var isHttps = clientRequest.primary_request.split(" ")
+        var address = isHttps[0];
+        if(isHttps = isHttps[1].split(":") !== undefined) {
+          if (isHttps[1] == 443) {
+            console.log("ITS HTTPS HAi");
+
+          }
+        }
+
+        console.log(isHttps);
+        //Here, after parsing, decide the request type and
+        //console.log('  ** START ** ');
+        console.log('<< From client to proxy ', msg.toString());
+        var serviceSocket = new net.Socket();
+
+
+        serviceSocket.connect(parseInt(80), REMOTE_ADDR, function () {
+            //console.log('>> From proxy to remote', msg.toString());
+            serviceSocket.write(msg);
+        });
+        serviceSocket.on("data", function (data) {
+            //console.log('<< From remote to proxy', data.toString());
+            socket.write(data);
+            //console.log('>> From proxy to client', data.toString());
+        });
+        serviceSocket.on("error", function (data) {
+            //console.log('<< From remote to proxy', data.toString());
+            console.log(data);
+            //console.log('>> From proxy to client', data.toString());
+        });
+
+    });
+
+});
+httpServer.listen(6512);
+
+function parseClientRequest(msg) {
+  //Json object to be returned with all header data encapsulated
+  var clientRequest = {};
+  //Split header content
+  toParse = msg.toString();
+  array = toParse.split('\r\n')
+  //Remove primary request url
+  clientRequest.primary_request = array[0]
+  //Build JSON object
+  var i;
+  var item = "";
+  var attribute = "";
+  var value = "";
+  for (i = 1; i < array.length - 1; i++) {
+    item = array[i];
+    item = item.split(": ");
+    attribute = item[0];
+    value = item[1];
+    clientRequest[attribute] = value;
+  }
+  //console.log(clientRequest);
+  return clientRequest;
+}
